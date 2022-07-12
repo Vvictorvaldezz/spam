423c78e06e5db5b940879641dae5f602b3f9b945
Vvictorvaldezz <61363942+Vvictorvaldezz@users.noreply.github.com>
2022-07-11 20:27:06 -0500

    agregando

--- src/app.py
+++ src/app.py
@@ -1,18 +1,20 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 from flask import Flask, render_template
+
+from matplotlib import pyplot as plt
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+
 import io
 from flask import Response
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
-
 
 app = Flask(__name__)
 
-url_datos = 'https://raw.githubusercontent.com/Rodrigo-JM108/Datos_IA/master/datos_ml.csv'
+url_datos = 'https://raw.githubusercontent.com/Vvictorvaldezz/proySpam/master/csv/kddt.csv'
+
 dataset = pd.read_csv(url_datos)
 
 @app.route("/")
@@ -21,41 +23,38 @@ def home():
 
 
 @app.route("/prediccion", methods=['POST'] )
-def prediccion():
-    
 
+def prediccion():
     X = dataset[['duration', 'count']]
     Y = dataset['class']
-
     correos = dataset.groupby('class').size()
-
     X_train,X_test,y_train,y_test = train_test_split(X,Y,test_size=0.3,random_state=0)
-
     modelo_RL = linear_model.LogisticRegression()
-
     modelo_RL.fit(X_train,y_train)
-
-
     y_pred = modelo_RL.predict(X_test)
-
     p_s = round(metrics.accuracy_score(y_test, y_pred),3)
+    
     return render_template('index.html', prediccion=f'La predicción es: {p_s}', num_correos=f'{correos}')
 
+
+
 @app.route('/grafica')
+
 def dibuja_grafico():
     x_values = dataset['class'].unique()
     y_values = dataset['class'].value_counts().tolist()
     plt.bar(x_values, y_values)
-    plt.title('Detección de SPAM')
+    plt.title('SPAM')
     ax = plt.subplot()                   
     ax.set_xticks(x_values)            
     ax.set_xticklabels(x_values)        
-    ax.set_xlabel('Tipo de correo')  
-    ax.set_ylabel('Cantidad de correos') 
+    ax.set_xlabel('Correo')  
+    ax.set_ylabel(' Numero de correos') 
     output = io.BytesIO()
     plt.legend()
     plt.savefig(output, format='png') 
     return Response(output.getvalue(), mimetype='image/png')
+
 
 if __name__ == "__main__":
     app.run()
